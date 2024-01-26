ae2d2ec7128b596fee8ee7dd89f2f12901df2bdb
akshay7745 <akshay7745@gmail.com>
2024-01-26 09:13:20 +0530

    "Project compeleted (tried second time)"

---
+++ promiseProjectPart1/index.js
@@ -0,0 +1,44 @@
+const posts =[{title:"Post 1"},{title:"Post 2"}];
+const user ={name:"akshay",lastTimeActivityTime: new Date().toTimeString()}
+console.log(user.lastTimeActivityTime);
+
+function createPost (){
+    return new Promise((res,rej)=>{
+        posts.push({title:"Post 3"});
+        res(posts);
+    })
+}
+function deletePost (){
+    return new Promise((res,rej)=>{
+        setTimeout(()=>{
+            res(posts.pop())
+
+        },5000)
+       
+    })
+}
+
+function updateLastUserActivityTime(){
+ return new Promise((res,rej)=>{
+    setTimeout(()=>{
+        user.lastTimeActivityTime = new Date().toTimeString();
+        res(user.lastTimeActivityTime);
+    },2000)
+ })
+}
+\
+
+function userCreatedPost (){
+    Promise.all([createPost(),updateLastUserActivityTime()]).then((values)=>{
+    console.log(values);
+    console.log(values[1])
+});
+}
+function userDeletedPost (){
+    Promise.all([deletePost(),updateLastUserActivityTime()]).then((values)=>{
+    console.log(values);
+    console.log(values[1])
+});
+}
+userCreatedPost();
+userDeletedPost();
